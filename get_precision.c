744314e22c871a2abf20e879e7b2b4e51dbfac0b
vbox <122890435+vbox163@users.noreply.github.com>
2023-04-03 08:43:02 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/**
+/** @VBOX163
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
- * Return: Precision.
+ * Return: Precision @VBOX163
  */
 int get_precision(const char *format, int *i, va_list list)
 {

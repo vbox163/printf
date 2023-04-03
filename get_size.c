43a4a03bfb4618ddd9ca2cc697533668cdc1ba3b
vbox <122890435+vbox163@users.noreply.github.com>
2023-04-03 08:43:19 +0100

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/**
+/** @VBOX163
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: Precision @VBOX163
  */
 int get_size(const char *format, int *i)
 {

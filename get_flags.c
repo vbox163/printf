38ddf2a759ac113e1a0e684585cbe6a22c02ddb2
vbox <122890435+vbox163@users.noreply.github.com>
2023-04-03 08:42:39 +0100

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/**
+/** @VBOX163
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
- * Return: Flags:
+ * Return: Flags: @VBOX163
  */
 int get_flags(const char *format, int *i)
 {
